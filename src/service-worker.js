37891b6e20b80834c22bf88280fb41c15350bbc0
Rizki Ardian <radeite8@gmail.com>
2023-07-25 07:58:55 +0700

    Remove root from precached

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,7 @@
 import { build, files, prerendered, version } from '$service-worker';
 import { precacheAndRoute } from 'workbox-precaching';
 
-const precache_list = [
-	'/', // Attention: serves stale index, might not be ideal for your use case.
-	...build,
-	...files,
-	...prerendered
-].map((s) => ({
+const precache_list = [...build, ...files, ...prerendered].map((s) => ({
 	url: s,
 	revision: version
 }));
