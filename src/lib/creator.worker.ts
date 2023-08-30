f93bae76bb64b6bac7c45c60ede1e7a4972ccb5c
Rizki Ardian <radeite8@gmail.com>
2023-08-30 10:52:01 +0700

    Use simple for loop in creator worker

--- src/lib/creator.worker.ts
+++ src/lib/creator.worker.ts
@@ -1,7 +1,10 @@
 import * as Comlink from 'comlink';
 
 async function create(qty: number, creator: () => object): Promise<Uint8Array> {
-	const results = await Promise.all(Array.from(Array(qty)).map(creator));
+	const results = [];
+	for (let i = 0; i < qty; i++) {
+		results[i] = await creator();
+	}
 	const encoder = new TextEncoder();
 	const arr = encoder.encode(JSON.stringify(results));
 	return Comlink.transfer(arr, [arr.buffer]);
