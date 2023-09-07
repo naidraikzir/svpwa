41311b7b58037def17e880dde44c82024e2f488a
Rizki Ardian <radeite8@gmail.com>
2023-09-07 19:03:12 +0700

    Add menu hint

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -5,3 +5,8 @@
 <DocumentHead title="Home" description="Home" />
 
 Home sweet home...
+
+<div class="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center text-2xl">
+	<div>Menu</div>
+	<div>↘️</div>
+</div>
