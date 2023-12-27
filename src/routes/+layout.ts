399fb6651ac0c68764e0396fb7443833b1cfbd8c
Rizki Ardian <radeite8@gmail.com>
2023-12-27 11:52:55 +0700

    Update dependencies

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,9 +5,9 @@ export const ssr = false;
 
 export const load = ({ route }) => {
 	if (route.id?.startsWith('/(protected)') && !localStorage.token) {
-		throw redirect(302, '/login');
+		redirect(302, '/login');
 	} else if (route.id?.startsWith('/login') && !!localStorage.token) {
-		throw redirect(302, '/');
+		redirect(302, '/');
 	}
 
 	return {};
